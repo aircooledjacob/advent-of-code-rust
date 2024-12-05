0aafc0863628df2ee1b23d6aea6a8dde413576fa
Jacob Neave <35272149+aircooledjacob@users.noreply.github.com>
2024-12-05 20:56:48 +0000

    add looping to menu, and further improve structure of menu system

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,21 @@ use advent_of_code_rust::year2022;
 use inquire::Select;
 
 fn main() {
+    
     let years: Vec<&str> = vec!["Exit", "2022", "2024"];
-    let selected_year: &str = Select::new("Which year would you like to run?", years)
-        .prompt()
-        .expect("There was an error, please try again");
+    let mut user_requested_exit = false;
+    
+    while !user_requested_exit {
+        let selected_year: &str = Select::new("Which year would you like to run?", years.clone())
+            .prompt()
+            .expect("There was an error, please try again");
 
-    let days: Vec<u8> = (1..25).collect();
-    let selected_day = Select::new("Which day would you like to run", days)
-        .prompt()
-        .expect("There was an error, please try again");
-
-    match selected_year {
-        "2022" => match selected_day {
-            1 => year2022::day1::run(),
-            2 => year2022::day2::run(),
-            3 => year2022::day3::run(),
-            4 => year2022::day4::run(),
-            
-            _ => panic!("Unknown day selected / day not implemented yet"),
-        },
-        "2024" => {}
-        "Exit" => (),
-        _ => panic!("Unknown year selected"),
+        match selected_year {
+            "2022" => year2022::menu(),
+            "2024" => (),
+            "Exit" => user_requested_exit = true,
+            _ => panic!("Unknown year selected"),
+        }
     }
+
 }
--- src/year2022/mod.rs
+++ src/year2022/mod.rs
@@ -1,5 +1,23 @@
+use inquire::Select;
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
 mod dayx;
+
+pub fn menu() {
+    let days: Vec<u8> = (1..5).collect();
+    let selected_day = Select::new("Which day would you like to run", days)
+        .prompt()
+        .expect("There was an error, please try again");
+
+    match selected_day {
+        1 => day1::run(),
+        2 => day2::run(),
+        3 => day3::run(),
+        4 => day4::run(),
+
+        _ => panic!("Unknown day selected / day not implemented yet"),
+    }
+}
